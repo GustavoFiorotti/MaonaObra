17c34460c36ac0c50fe48e8e6e1b0cbf8f961202
Gustavo Fiorotti <gustavo.fiorotti8@hotmail.com>
2025-09-16 00:45:31 -0300

    fix: corrige importações e configuração do Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
-  import { defineConfig } from 'vite';
-  import react from '@vitejs/plugin-react-swc';
-  import path from 'path';
-
-  export default defineConfig({
-    plugins: [react()],
-    resolve: {
-      extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-      
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    build: {
-      target: 'esnext',
-      outDir: 'dist',
+  },
+  build: {
+    target: "esnext",
+    outDir: "build",
+    commonjsOptions: {
+      transformMixedEsModules: true,
     },
-    server: {
-      port: 3000,
-      open: true,
-    },
-  });
+  },
+  server: {
+    port: 3000,
+    open: true,
+  },
+});
